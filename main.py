7f674f3b08fb9a14c0b388f1fa4bb2d09b519ce6
verhovv <vladimirverhovl@yandex.ru>
2022-11-13 01:25:44 +1000

    рабочие пятнашки

---
+++ main.py
@@ -0,0 +1,86 @@
+import sys
+
+from settings import *
+from plate import Plate
+
+
+class SettingWindow(SettingsWindow):
+    def onPress(self):
+        super(SettingWindow, self).onPress()
+        Window.make_plates(window)
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.time = QLabel('Время: 0', self)
+        self.time.resize(150, 25)
+        self.time.setFont(MAIN_FONT)
+        self.time.move(10, 20)
+
+        self.record = QLabel(f'Рекорд: {record()}', self)
+        self.record.resize(150, 25)
+        self.record.setFont(MAIN_FONT)
+        self.record.move(self.time.x(), self.time.y() + 15)
+
+        self.setWindowTitle("PyQT Barley-Break")
+
+        self.setMaximumSize(QSize(*WINDOW_SIZE))
+        self.setMinimumSize(QSize(*WINDOW_SIZE))
+
+        self.setWindowIcon(QIcon(QPixmap(QImage('icon.png'))))
+
+        self.imageButton = QPushButton(self)
+
+        self.settingsButton = QPushButton('Настройки', self)
+        self.settingsButton.clicked.connect(self.show_setting_window)
+        self.settingsButton.move(WINDOW_WIDTH - self.settingsButton.width() - 10, 10)
+
+        self.settingWindow = SettingWindow()
+
+        self.main_image = self.settingWindow.chooseImage()
+
+        self.make_plates()
+
+        self.show()
+
+    def show_setting_window(self):
+        self.settingWindow.show()
+
+    def make_plates(self):
+        from settings import SIZE_OF_PLATE, plates_width, plates_height, main_image
+
+        self.mainImage = main_image
+        self.mainImage = self.mainImage.scaled(plates_width * SIZE_OF_PLATE, plates_height * SIZE_OF_PLATE)
+
+        self.imageButton.resize(plates_width * SIZE_OF_PLATE // 2, plates_height * SIZE_OF_PLATE // 2)
+        self.imageButton.move(self.width() // 2 - self.imageButton.width() // 2, 50)
+
+        self.imageButton.setIcon(QIcon(QPixmap(self.mainImage)))
+        self.imageButton.setIconSize(self.imageButton.size())
+
+        Plate.clear_plates()
+
+        for x in range(plates_width):
+            for y in range(plates_height):
+                pixmap = QPixmap(
+                    self.mainImage.copy(QRect(x * SIZE_OF_PLATE, y * SIZE_OF_PLATE, SIZE_OF_PLATE, SIZE_OF_PLATE))
+                )
+
+                Plate(self, [x, y], pixmap)
+
+        Plate.plates[-1].setParent(None)
+        Plate.plates = Plate.plates[:-1]
+
+        Plate.generate_plates()
+        Plate.set_enabled(True)
+
+        self.time.setText(f'Время: 0 секунд')
+        self.record.setText(f'Рекорд: {record(plates_width * plates_height)}')
+
+
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    window = Window()
+    sys.exit(App.exec())
---
+++ plate.py
@@ -0,0 +1,173 @@
+import random
+
+from settings import *
+
+import time
+
+
+class Plate(QPushButton):
+    is_started = False
+    is_won = False
+
+    # list of all plates
+    plates: list['Plate'] = list()
+
+    def __init__(self, surface: QWidget, coords: list[int, int], pixmap: QPixmap):
+        from settings import SIZE_OF_PLATE
+
+        super().__init__(surface)
+
+        self.surface = surface
+
+        self.pixmap = pixmap
+
+        self.coords = coords
+        self.first_coords = self.coords.copy()
+
+        self.resize(SIZE_OF_PLATE + PADDING, SIZE_OF_PLATE + PADDING)
+        self.move_to_home()
+
+        self.distance = (self.coords[0] - empty_plate_coords[0], self.coords[1] - empty_plate_coords[1])
+
+        self.setIcon(QIcon(self.pixmap))
+        self.setIconSize(self.pixmap.rect().size())
+
+        self.clicked.connect(self.onClick)
+
+        self.start_time = time.time()
+
+        Plate.plates.append(self)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.showTime)
+
+        self.second_count = 0
+
+    # moving plate to its coordinates
+    def move_to_home(self):
+        from settings import plates_width, plates_height
+
+        self.move(self.surface.width() // 2 + self.coords[0] * self.width() - self.width() * plates_width // 2,
+                  self.surface.height() // 5 * 4 + self.coords[
+                      1] * self.height() - self.height() * plates_height // 5 * 4)
+
+    # animated moving
+    def move_on(self, x, y) -> None:
+        # animation properties
+        self.anim = QPropertyAnimation(self, b"geometry")
+        self.anim.setDuration(100)
+        self.anim.setStartValue(self.geometry())
+        self.anim.setEndValue(QRect(x, y, self.width(), self.height()))
+
+        # starting the animation
+        self.anim.start()
+
+    # returns True if can move
+    def can_move(self) -> bool:
+        # checking that distance is less or equals than 1
+        for i in range(2):
+            if abs(self.distance[i]) > 1:
+                return False
+
+        # checking the 1:-1 distance cases
+        if abs(self.distance[0]) == abs(self.distance[1]):
+            return False
+
+        return True
+
+    def onClick(self) -> None:
+        from settings import empty_plate_coords
+
+        self.distance = (self.coords[0] - empty_plate_coords[0], self.coords[1] - empty_plate_coords[1])
+
+        if not self.can_move():
+            return
+
+        for i in range(2):
+            self.coords[i] -= self.distance[i]
+            empty_plate_coords[i] += self.distance[i]
+
+        self.move_on(self.x() - self.width() * self.distance[0], self.y() - self.height() * self.distance[1])
+
+        if self.check_is_won():
+            Plate.is_won = True
+
+        if not Plate.is_started:
+            self.timer.start(100)
+            Plate.is_started = True
+
+    def showTime(self) -> None:
+        from settings import plates_height, plates_width
+        if Plate.is_won:
+            Plate.set_enabled(False)
+
+            if record(plates_height * plates_width) > self.second_count / 10 or record(
+                    plates_height * plates_width) == 0:
+                cur.execute(f"""INSERT INTO records
+                VALUES({plates_width * plates_height}, {float(self.second_count / 10)})""")
+                database.commit()
+
+                self.surface.record.setText(f"Рекорд: {self.second_count / 10}")
+
+                QMessageBox.about(self, 'Вы выиграли!!! НОВЫЙ РЕКОРД!!!',
+                                  f'Победа! Время: {self.second_count / 10} секунд!'
+                                  f'\nНовый рекорд!')
+            else:
+                QMessageBox.about(self, 'Вы выиграли!!!', f'Победа! Время: {self.second_count / 10} секунд!')
+            self.second_count = 0
+
+        if not Plate.is_started or Plate.is_won:
+            self.timer.stop()
+            return
+
+        self.second_count += 1
+        self.surface.time.setText(f'Время: {self.second_count / 10} секунд')
+
+    # checking if won
+    @staticmethod
+    def check_is_won() -> bool:
+        if tuple(map(lambda x: x.first_coords, Plate.plates)) == tuple(map(lambda x: x.coords, Plate.plates)):
+            return True
+        return False
+
+    @staticmethod
+    def set_enabled(enabled: bool) -> None:
+        for plate in Plate.plates:
+            plate.setEnabled(enabled)
+
+    @staticmethod
+    def clear_plates() -> None:
+        for plate in Plate.plates:
+            plate.setParent(None)
+        Plate.plates.clear()
+
+    @staticmethod
+    # generating new plates
+    def generate_plates() -> None:
+        from settings import plates_width, plates_height
+
+        Plate.is_won = False
+        Plate.is_started = False
+
+        coords = list(([x, y] for x in range(plates_width) for y in range(plates_height)))[:-1]
+
+        k = 0 if plates_height % 2 else 1
+        while (not k % 2) if plates_height % 2 else k % 2:
+            k = 0
+            random.shuffle(coords)
+
+            for i in range(len(coords)):
+                for j in range(i + 1, len(coords)):
+                    if coords[i] > coords[j]:
+                        k += 1
+            k += plates_height
+            print(k)
+        print(k)
+
+        for i in range(len(Plate.plates)):
+            Plate.plates[i].coords = coords[i]
+            Plate.plates[i].move_to_home()
+            Plate.plates[i].show()
+
+        empty_plate_coords[0] = plates_width - 1
+        empty_plate_coords[1] = plates_height - 1
